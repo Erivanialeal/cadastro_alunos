0b92b6450f4dee42f8e6129f33d97c3c1c6b4b56
Erivania Leal <erivanial.s1507@gmail.com>
2026-04-23 19:19:14 -0300

    WIP: ajustes no AlunosRepository querys não finalizadas

--- src/main/java/com/erivania/repository/AlunosRepository.java
+++ src/main/java/com/erivania/repository/AlunosRepository.java
@@ -6,6 +6,7 @@ import com.model.Aluno;
 import com.model.Curso;
 
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 public class AlunosRepository {
 
@@ -71,4 +72,71 @@ public class AlunosRepository {
                 .setParameter("nome", "%" + nome + "%")
                 .getResultList();
     }
+
+    public Long contarQuantosAlunosExistem() {
+        return em.createQuery("SELECT COUNT(a) FROM Aluno a", Long.class)
+                .getSingleResult();
+    }
+
+    public List<Object[]> contarAlunosPorCurso() {
+        return em.createQuery("SELECT a.curso.nome, COUNT(a)" +
+                "FROM Aluno a" +
+                "GROUP BY a.curso.nome", Object[].class)
+                .getResultList();
+    }
+
+    public List<Aluno> buscarAlunosComSeusCursos(String nome) {
+        return em.createQuery("SELECT a FROM Aluno a JOIN FETCH a.curso c" +
+                " WHERE c.nome = :nome", Aluno.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Aluno> buscarAlunosPorCursoEspecifico(Long idCurso) {
+        return em.createQuery("SELECT a FROM Aluno a JOIN FETCH a.curso c" +
+                " WHERE c.id = :idCurso", Aluno.class)
+                .setParameter("idCurso", idCurso)
+                .getResultList();
+    }
+
+    public List<Curso> buscarCursosComSeusAlunos(String Alunonome) {
+        return em.createQuery("SELECT DISTINCT c FROM Curso c JOIN FETCH c.alunos a" +
+                "WHERE a.nome = :nome", Curso.class)
+                .setParameter("nome", Alunonome)
+                .getResultList();
+    }
+
+    public List<Aluno> buscarAlunoDeUmCursoComNomeEspecifico(String nomeCurso) {
+        return em.createQuery("SELECT a FROM Aluno a" +
+                "JOIN a.curso c" +
+                "WHERE c.nome = :nomeCurso", Aluno.class)
+                .setParameter("nomeCurso", nomeCurso)
+                .getResultList();
+    }
+
+    public List<Curso> BuscarCursosDeProfessoresEspecíficos(String nomeProfessor) {
+        return em.createQuery("SELECT c FROM Curso c" +
+                "JOIN c.professores p " +
+                "WHERE p.nome = :nomeProfessor", Curso.class)
+                .setParameter("nomeProfessor", nomeProfessor)
+                .getResultList();
+    }
+
+    @Transactional
+    public int atualizarEmailDeAluno(Long id, String email) {
+        return em.createQuery("UPDATE Aluno a SET a.email = :email WHERE a.id = :id")
+                .setParameter("email", email)
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    public int atualizarCursoDeUmAluno(Long nomeAluno, String novoCurso) {
+        return em.createQuery("UPDATE Curso c SET c.nome = : novoCurso" +
+                "WHERE c.id = (SELECT a.curso.id FROM Aluno a WHERE a.nome = :nomeAluno")
+                .setParameter("idAluno", nomeAluno)
+                .setParameter("novoCurso", novoCurso)
+                .executeUpdate();
+
+    }
+
 }
